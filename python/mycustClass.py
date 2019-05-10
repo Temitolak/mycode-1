1f148b80abba69232394a5bc06df57a7cbf4b241
zach <rzfeeser@alta3.com>
2019-05-10 13:24:29 -0400

    Final commit for the week

---
+++ python/mycustClass.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Dog:
+    def __init__(self, name, age):
+        self.n = name
+        self.a = age
+
+    def __str__(self):
+        return 'Name: ' + self.n + ' Age: ' + str(self.a)
+
+    def aged(self, yearsolder):
+        self.a = self.a + yearsolder
+
+class Jackrussell(Dog):
+    def __init__(self, name, age, isWirehair, isHunter):
+        Dog.__init__(self, name, age)
+        self.wh = isWirehair
+        self.h = isHunter
+
+    def __str__(self):
+        retStr = Dog.__str__(self)
+        retStr += ' Wirehaired: ' + str(self.wh) + ' Trained to hunt: ' + str(self.h)
+        return retStr
+
+def main():
+    muttley = Dog("Fido", 3)
+    print(muttley)
+
+    muttley.aged(1)
+
+    print(muttley)
+
+    eddie = Jackrussell('Eddie', 4, False, True)
+    print(eddie)
+
+    eddie.aged(3)
+    print(eddie)
+
+main()
